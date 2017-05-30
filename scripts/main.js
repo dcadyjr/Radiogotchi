079bdae0877ec64d1c45c7e76ad7569d34015d0a
David Cady Jr <davidcadyjr@mac.com>
2017-05-30 16:51:25 -0500

    added almost dying face and dead image

--- scripts/main.js
+++ scripts/main.js
@@ -6,10 +6,13 @@ var bear = {
 	boredom: 5,
 	sleepiness: 5,
 	feed: function(){
-		alert(this.name + " says tasty!");
+		alert("tasty!");
 		this.hunger = this.hunger - 1;
 	}
 };
+//Global variables
+var image = document.getElementById("start");//gets the image element
+
 var hungerMeter = document.getElementById("hunger");
 hungerMeter.innerText = bear.hunger;
 
@@ -21,6 +24,9 @@ sleepinessMeter.innerText = bear.sleepiness;
 
 var nameButton = document.getElementById("nameButton");//gets name button element
 nameButton.addEventListener("click", function(){//adds click event listener
+
+//end Global variables
+
   var name = document.getElementById("bearName").value;//stores name value from input box
   bear.name = name;//puts the name value into the bear object
   alert("Why don't you remember my name!")//alert
@@ -45,11 +51,17 @@ setInterval(
 	3000);
 
 var almostDeadFace = function (){
-	var image = document.getElementById("start");//gets the image element
-	if (bear.hunger > 6) {// if the score is greater than 6
-	image.setAttribute("src", "https://s-media-cache-ak0.pinimg.com/236x/b5/da/ab/b5daaba84f15204e39afbc3462c4004b.jpg");//changes the image source attribute to a new image
-		}
+	if (bear.hunger > 6 && bear.hunger < 10) {// if the score is greater than 6
+	  image.setAttribute("src", "https://s-media-cache-ak0.pinimg.com/236x/b5/da/ab/b5daaba84f15204e39afbc3462c4004b.jpg");//changes the image source attribute to a new image
+	} else if (bear.hunger >= 10) {
+		image.setAttribute("src", "https://images-na.ssl-images-amazon.com/images/I/41DMiDVRWeL._AC_UL320_SR250,320_.jpg");
+	}
+		else {
+			image.setAttribute("src", "http://emblemsbf.com/img/1379.jpg")
+	}
 }
 
-var sadDeadFace = function ()
+// var sadDeadFace = function (){
+// 	if (bear.)
+// }
 
